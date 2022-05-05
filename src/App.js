61818eb900559cab175655d86ecb5887f3e95584
programer1983 <programer972@gmail.com>
2022-05-05 16:58:07 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import {useState, useRef} from "react"
+
+const users = [
+  {id: 1, name: "Bill"},
+  {id: 2, name: "Kelly"},
+  {id: 3, name: "Harry"},
+]
 
 function App() {
+
+
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <input type="text" />
+      <button type="button">Search</button>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li>{user.name}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
