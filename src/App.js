4eb33b52c2a34968db45e27d17665520d8f95f62
programer1983 <programer972@gmail.com>
2022-05-05 17:30:03 +0300

    commit-4;

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import './App.css';
 import {useState, useRef} from "react"
 
 const users = [
-  {id: 1, name: "Bill"},
-  {id: 2, name: "Kelly"},
-  {id: 3, name: "Harry"},
+  {id: "1", name: "Bill"},
+  {id: "2", name: "Kelly"},
+  {id: "3", name: "Harry"},
 ]
 
 function App() {
+  const [activeUsers, setActiveUsers] = useState(users)
+  const inputRef = useRef(null)
 
+  const onSearchClick = () => {
+    const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(inputRef.current.value.toLowerCase()))
+    setActiveUsers(filteredUsers)
+  }
 
   return (
     <div className="App">
-      <input type="text" />
-      <button type="button">Search</button>
+      <input type="text" ref={inputRef} />
+      <button type="button" onClick={onSearchClick}>Search</button>
       <ul>
-        {users.map((user) => {
+        {activeUsers.map((user) => {
           return (
             <li>{user.name}</li>
           )
